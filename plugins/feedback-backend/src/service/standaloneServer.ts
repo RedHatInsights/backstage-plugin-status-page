247e81455dbca305c463801fa42278d346d44a02
Yash Oswal <yoswal@redhat.com>
2023-09-26 10:40:38 +0530

    fixed yarn tsc errors

--- plugins/feedback-backend/src/service/standaloneServer.ts
+++ plugins/feedback-backend/src/service/standaloneServer.ts
@@ -1,7 +1,9 @@
-import { createServiceBuilder } from '@backstage/backend-common';
+import { HostDiscovery, createServiceBuilder } from '@backstage/backend-common';
 import { Server } from 'http';
-import { Logger } from 'winston';
+import { Logger, createLogger, transports } from 'winston';
 import { createRouter } from './router';
+import { Config, ConfigReader } from '@backstage/config';
+import { DiscoveryService } from '@backstage/backend-plugin-api';
 
 export interface ServerOptions {
   port: number;
@@ -12,10 +14,16 @@ export interface ServerOptions {
 export async function startStandaloneServer(
   options: ServerOptions,
 ): Promise<Server> {
-  const logger = options.logger.child({ service: 'feedback-backend' });
+  const config: Config = new ConfigReader({});
+  const discovery: DiscoveryService = HostDiscovery.fromConfig(config);
+  const logger: Logger = createLogger({
+    transports: [new transports.Console({ silent: true })],
+  });
   logger.debug('Starting application server...');
   const router = await createRouter({
-    logger,
+    logger: logger,
+    config: config,
+    discovery: discovery,
   });
 
   let service = createServiceBuilder(module)
